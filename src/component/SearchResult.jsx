91d7cbe8d83bcc6449d55683d57982f5896c3ac4
Azka Nurfadillah <azkanurfadillah@gmail.com>
2021-10-07 17:58:52 +0700

    update styling

--- src/component/SearchResult.jsx
+++ src/component/SearchResult.jsx
@@ -32,53 +32,49 @@ const SearchResult = () => {
 
     return (
         <Box>
-            <Flex wrap="wrap" gridRowGap="6" gridColumnGap="4" my="90px">
+            <Flex wrap="wrap" gridRowGap="6" gridColumnGap="4" my="102px">
                 {movies?.Search.length ?
                     movies.Search.map(movie => (
                         <Flex key={movie.imdbID}
-                            flex="1" direction="column"
-                            minW="198px" maxW="232px" bgColor="twitter.100" p="4" borderRadius="md" >
+                            _hover={{ cursor: "pointer" }}
+                            flex="1" direction="column" position="relative"
+                            minW="198px" maxW="232px" bgColor="twitter.100" p="4" borderRadius="md"
+                            onMouseEnter={() => setViewDetails({ status: true, id: movie.imdbID })}
+                            onMouseLeave={() => setViewDetails({ status: false, id: "" })}
+                        >
                             <Image
                                 width="100%"
-                                mx="auto"
                                 objectFit="cover"
                                 src={movie.Poster}
                                 onLoad={() => setLoadedImage(prevSate => [...prevSate, movie.imdbID])}
                                 display={loadedImage.includes(movie.imdbID) ? "block" : "none"}
-                                _hover={{ cursor: "pointer" }}
                                 onClick={() => setModal({ status: true, imageURL: movie.Poster })}
                             />
                             {
                                 !loadedImage.includes(movie.imdbID) && <Skeleton borderRadius="md" speed={.5} fadeDuration={.8} height="200px" />
                             }
-                            <Box position="relative"
-                                onMouseEnter={() => setViewDetails({ status: true, id: movie.imdbID })}
-                                onMouseLeave={() => setViewDetails({ status: false, id: "" })}
-                                _hover={{ cursor: "pointer" }}
-                            >
-                                <Box >
-                                    <Flex justify="space-between">
-                                        <Text>{movie.Type}</Text>
-                                        <Text>{movie.Year}</Text>
-                                    </Flex>
-                                    <Text>{movie.Title}</Text>
-                                </Box>
-                                {viewDetails.id === movie.imdbID &&
-                                    <Slide direction="bottom" in={true} style={{ zIndex: 10, position: "absolute", bottom: "-15px", left: "-15px", right: "-15px" }}  >
-                                        <Flex
-                                            align="center"
-                                            h="72px"
-                                            p="4"
-                                            color="white"
-                                            bgColor="rgba(26, 32, 44, .7)"
-                                            borderRadius="md"
-                                            onClick={() => history.push(`/omdb/${movie.Title}`)}
-                                        >
-                                            <Text fontWeight="bold" fontSize="md">Details <ArrowForwardIcon w={6} h={6} /> </Text>
-                                        </Flex>
-                                    </Slide>
-                                }
+                            <Box >
+                                <Flex justify="space-between">
+                                    <Text>{movie.Type}</Text>
+                                    <Text>{movie.Year}</Text>
+                                </Flex>
+                                <Text>{movie.Title}</Text>
                             </Box>
+                            {viewDetails.id === movie.imdbID &&
+                                <Slide direction="bottom" in={true} style={{ zIndex: 10, position: "absolute", bottom: "0", left: "0", right: "0" }}  >
+                                    <Flex
+                                        align="center"
+                                        h="72px"
+                                        p="4"
+                                        color="white"
+                                        bgColor="rgba(26, 32, 44, .7)"
+                                        borderRadius="md"
+                                        onClick={() => history.push(`/omdb/${movie.Title}`)}
+                                    >
+                                        <Text fontWeight="bold" fontSize="md">Details <ArrowForwardIcon w={6} h={6} /> </Text>
+                                    </Flex>
+                                </Slide>
+                            }
                         </Flex>
                     ))
                     : <Text>{error?.Error || movies?.Error}</Text>
